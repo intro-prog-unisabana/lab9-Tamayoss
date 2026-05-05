688a96d2f5b93b00d1af97998e36d763e5c7a89b
Emilio Tamayo <emiliotase@unisabana.edu.co>
2026-05-04 22:42:41 -0500

    ee

--- utils.py
+++ utils.py
@@ -7,12 +7,12 @@ def person_data():
     while True:
         account_number=int(input("Enter a 4-digit account number:\n"))
         balance=input("Enter the initial balance:\n")
-        my_bank_a=BankAccount(account_number, balance)
+        my_bank_a= BankAccount(account_number, balance)
         my_person.add_account(my_bank_a)
         entrada=input("Are you done adding accounts? (yes/no):")
         if entrada == "yes":
             break
-    return my_person
+    return my_person    
 def balance_summary(person_list: list):
     for person in person_list:
         total_balance = sum(account.balance for account in person.accounts)
